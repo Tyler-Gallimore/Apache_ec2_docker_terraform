2cbb59d96efac5286d142d72d5d9062680a4dbf3
Tyler-Gallimore <lifforn@mailfence.com>
2023-10-09 19:49:14 -0400

    Fixed vpc_security_group_ids in EC2

--- ec2.tf
+++ ec2.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "web_server" {
     ami = var.ec2_ami
     instance_type = var.ec2_instance_type
-    vpc_security_group_ids = [aws_security_group.allow_http]
+    vpc_security_group_ids = [aws_security_group.allow_http.id]
     availability_zone = "us-east-1a"
 
     user_data = <<-EOF
