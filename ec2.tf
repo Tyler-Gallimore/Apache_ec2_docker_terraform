3c7d0f23f9d32f9dde4acb8ec5d703226032f482
Tyler-Gallimore <lifforn@mailfence.com>
2023-10-14 20:31:28 +0000

    Finished code

--- ec2.tf
+++ ec2.tf
@@ -1,24 +1,28 @@
 resource "aws_instance" "web_server" {
-    ami = var.ec2_ami
-    instance_type = var.ec2_instance_type
-    vpc_security_group_ids = [aws_security_group.allow_http.id]
-    availability_zone = "us-east-1a"
+  ami                    = var.ec2_ami
+  instance_type          = var.ec2_instance_type
+  vpc_security_group_ids = [aws_security_group.allow_http.id]
+  availability_zone      = "us-east-1a"
 
-    user_data = <<-EOF
+  user_data = <<-EOF
     #!/bin/bash
     sudo yum update -y
-    sudo -y install firewalld
+    sudo yum install firewalld -y
+    sudo systemctl start firewalld
+    sudo systemctl enable firewalld
     sudo firewall-cmd --permanent --add-service=http
     sudo firewall-cmd --permanent --add-service=https
     sudo firewall-cmd --reload
     sudo yum install -y docker
+    sudo systemctl start docker
+    sudo systemctl enable docker
     sudo usermod -a -G docker ec2-user
     sudo docker pull httpd:latest
     sudo docker run --name apache_web -p 80:80 -d httpd
     EOF
 
 
-    tags = {
-        Name = "Webserver"
-    }
+  tags = {
+    Name = "Webserver"
+  }
 }
--- sg.tf
+++ sg.tf
@@ -1,30 +1,30 @@
 resource "aws_security_group" "allow_http" {
-    vpc_id = var.vpc_id
+  vpc_id = var.vpc_id
 
-    ingress {
-        description = "Inbound http traffic"
-        from_port = 80
-        to_port = 80
-        protocol = "tcp"
-        cidr_blocks = ["0.0.0.0/0"]
-    }
+  ingress {
+    description = "Inbound http traffic"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
-    ingress {
-        description = "HTTPS Traffic"
-        from_port = 443
-        to_port = 443
-        protocol = "tcp"
-        cidr_blocks = ["0.0.0.0/0"]
-    }
+  ingress {
+    description = "HTTPS Traffic"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
-    egress {
-        from_port = 0
-        to_port = 0
-        protocol = "-1"
-        cidr_blocks = ["0.0.0.0/0"]
-    }
-    
-    tags = {
-        Name = "allow_traffic"
-    }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "allow_traffic"
+  }
 }
